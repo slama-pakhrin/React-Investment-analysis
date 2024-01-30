7b44738f1af89e1c7ffb24e9bf1038e84214e649
slama-pakhrin <salonlama47@gmail.com>
2024-01-30 15:26:54 -0500

    Rendering condition for positive duration

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import UserInput from "./components/UserInput"
 import Results from "./components/Results"
 function App() {
   const [inputValues, setInputValues] = useState({
-    initialInvestment : 1220,
-    annualInvestment : 100,
-    expectedReturn: 6,
-    duration: 3
+    initialInvestment : 0,
+    annualInvestment : 0,
+    expectedReturn: 0,
+    duration: 1
 })
+
+const isDurationPositive = inputValues.duration >= 1;
+
 function handleChange(identifier, newValue){
     setInputValues((prevValues) => {
         return{ //we need other values when one of it is changing so we copy the object and replace the new element only
@@ -21,7 +24,8 @@ function handleChange(identifier, newValue){
     <>
    <Header />
    <UserInput onInputChange={handleChange} values={inputValues}/>
-   <Results userInput={inputValues}/>
+   {isDurationPositive ? <Results userInput={inputValues}/> : 
+   <p className="center">Please enter the positive duration. Thanks!</p>}
    </>
   )
 }
